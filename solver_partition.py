d41711c8e6f2485a48dff87135639d8851ac2769
mika <example.com>
2015-06-28 22:45:41 +0900

    求めるところまで行った。あとは、場所の番号を出力しないといけない…

--- solver_partition.py
+++ solver_partition.py
@@ -33,13 +33,7 @@ def find_exchange_pointer(city, common_index):
 def diff_distance(point1, common, point2):
     return distance(point1, common) + distance(common, point2) - distance(point1, point2)
 
-def connect_cities(city1, city2):
-    common_index1, common_index2, common = find_common_index(list(city1), list(city2))
-    pointer1_1, pointer1_2 = find_exchange_pointer(city1, common_index1);
-    pointer2_1, pointer2_2 = find_exchange_pointer(city2, common_index2);
-    pointer_list = [[pointer1_1, pointer2_1], [pointer1_1, pointer2_2], [pointer1_2, pointer2_1], [pointer1_2, pointer2_2]]
-
-    #find change_point
+def find_max_diff_point(pointer_list, city1, city2, common):
     max_diff = 0
     key = 0
     for i in range(4):
@@ -49,7 +43,17 @@ def connect_cities(city1, city2):
         if(diff > max_diff):
             max_diff = diff
             key = i
-    print key, max_diff
+    return key
+
+def connect_cities(city1, city2):
+    common_index1, common_index2, common = find_common_index(list(city1), list(city2))
+    pointer1_1, pointer1_2 = find_exchange_pointer(city1, common_index1);
+    pointer2_1, pointer2_2 = find_exchange_pointer(city2, common_index2);
+    pointer_list = [[pointer1_1, pointer2_1], [pointer1_1, pointer2_2], [pointer1_2, pointer2_1], [pointer1_2, pointer2_2]]
+
+    #find change_point
+    key = find_max_diff_point(pointer_list, city1, city2, common)
+    print key
 
     #extract common from city2. it should return path
     path = []
@@ -59,17 +63,19 @@ def connect_cities(city1, city2):
         for i in range(len(city2) - 1):
             print (point - i) % len(city2)
             path.append(city2[(point - i) % len(city2)])
-        print path
     else:
         point = pointer_list[key][1]
-        print "point",
-        print point
         for i in range(len(city2) - 1):
             path.append(city2[(point + i) % len(city2)])
-        print path
 
     #insert path to city1
-    
+    if(key < 2):
+        point = pointer_list[key][0] + 1
+        city1[point:point] = path
+    else:
+        point = pointer_list[key][0] - 1
+        city1[point:point] = path
+    return city1
 
 def solve(cities):
     
@@ -81,33 +87,13 @@ def solve(cities):
         first_harf_cities, latter_harf_cities = divide_cities(cities)
         result_first = solve(first_harf_cities)
         result_latter = solve(latter_harf_cities)
-        connect_cities(result_first, result_latter)
-
-    """
-    dist = [[0] * N for i in range(N)]
-    for i in range(N):
-        for j in range(N):
-            dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
-
-    current_city = 0
-    unvisited_cities = set(range(1, N))
-    solution = [current_city]
-
-    def distance_from_current_city(to):
-        return dist[current_city][to]
-
-    while unvisited_cities:
-        next_city = min(unvisited_cities, key=distance_from_current_city)
-        unvisited_cities.remove(next_city)
-        solution.append(next_city)
-        current_city = next_city
-"""
+        return connect_cities(result_first, result_latter)
     #return solution
     return cities
 
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
-    solve(read_input(sys.argv[1]))
+    print solve(read_input(sys.argv[1]))
     #solution = solve(read_input(sys.argv[1]))
     #print_solution(solution)
