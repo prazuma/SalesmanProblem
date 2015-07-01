8cf207591383036d8194d9ecde37255b313911f5
mika <example.com>
2015-07-01 13:03:47 +0900

    たぶん蟻さん完成

--- ant.py
+++ ant.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 import bisect
 
@@ -24,21 +25,21 @@ class Ant(object):
         while available_cities:
             self.next_city = self.select_next_city(available_cities)
             self.path.append(self.current_city)
-            self.path_len += distance(self.fields[self.current_city], self.fields[self.next_city])
+            self.path_len += self.fields['dists'][self.current_city][self.next_city]
             self.current_city = self.next_city
             available_cities.remove(self.next_city)
 
-        self.path_len += distance(self.fields[current_city], self.fields[initial_city])
+        self.path_len += self.fields['dists'][self.current_city][self.initial_city]
         self.path.append(self.current_city)
 
         return self.path
 
     def get_path_value(self, current_city, to_city):
-        pheromone = self.fields['pheromone'][current_city][to_city]
-        distance = distance(self.fields[current_city], self.fields[next_city])
-        return pheromone**self.alpha * ((1.0 / distance)**self.beta)
+        pheromon = self.fields['phers'][current_city][to_city]
+        distance = self.fields['dists'][self.current_city][to_city]
+        return pheromon ** self.alpha * ((1.0 / distance)**self.beta)
 
-    def cdf(self, weight):
+    def cdf(self, weight):#蟻が次にどこ行くかの式
         total = sum(weight)
         cdf_vals = []
         cumulation = 0
--- solver_ant.py
+++ solver_ant.py
@@ -7,10 +7,12 @@ import sys
 from ant import Ant
 from common import print_solution, read_input
 
-ALPHA = 1.0
-BETA = 5.0
-RHO = 0.5
-Q = 100
+ALPHA = 1.0#蟻が出すフェロモンの量
+BETA = 0.5#都市間距離の重要度を決めるパラメータ
+RHO = 0.5#フェロモンの蒸発率
+Q = 1
+
+MAX_RUN = 10 #この値が大きければ大きいほど値は良くなる
 
 def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
@@ -39,10 +41,47 @@ def get_ants(num_ant, fields):
     ants = []
     for i in range(num_ant):
         initial_city = numpy.random.randint(0, num_ant)
-        print initial_city
         ant = Ant(initial_city, fields, ALPHA, BETA)
         ants.append(ant)
     return ants
+
+def evaporate_pheromon(pheromon):
+    pheromon *= (1.0 - RHO)
+    return pheromon
+
+#蟻kが繰り返し回数tの時に落とすフェロモンの量
+def delta_pheromon(path_len):
+    delta = Q / path_len
+    return delta
+
+def update_pheromons(fields, ants):
+    
+    for i in range(len(fields['cities'])):
+        for j in range(i, len(fields['cities'])):
+            if i != j:
+                pheromon = evaporate_pheromon(fields['phers'][i][j])
+                fields['phers'][i][j] = fields['phers'][j][i] = pheromon
+                
+    for ant in ants:
+        for i in range(len(ant.path)):
+            f = ant.path[i]
+            t = ant.path[i - 1]
+            delta = delta_pheromon(ant.path_len)
+            fields['phers'][f][t] += delta
+            fields['phers'][f][t] = fields['phers'][t][f]
+
+def start(fields, ants):
+    best_ant = None
+    min_len = 1000000000
+    for i in range(MAX_RUN):
+        update_pheromons(fields, ants)
+        for ant in ants:
+            ant.run()
+            if ant.path_len < min_len:
+                best_ant = ant
+                min_len = ant.path_len
+    return best_ant
+
 
 def solve(cities):
     dists = get_dists(cities)
@@ -53,9 +92,10 @@ def solve(cities):
         phers = phers
     )
     ants = get_ants(len(cities), fields)
-    return ants
+    best_ant = start(fields, ants)
+    return best_ant
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
     solution = solve(read_input(sys.argv[1]))
-    #print_solution(solution)
+    print_solution(solution.path)
